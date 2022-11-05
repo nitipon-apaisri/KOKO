7438fddc23c9e8828932a7b71bd8e0ee1b2c238f
nitipon <nitipon.apaisri@gmail.com>
2022-11-05 21:04:05 +0100

    Added collection stats to MainProfileLayout

--- components/MainProfile/MainProfileLayout.tsx
+++ components/MainProfile/MainProfileLayout.tsx
@@ -1,6 +1,7 @@
 import { collectionObject } from "../../@types/collection";
 import { genetateProfileMedias } from "../../utils/modules";
 import styles from "../../styles/collectionInfo.module.css";
+import { Stats } from "../Stats";
 const MainProfileLayout = ({ data }: { data: collectionObject }) => {
     return (
         <div className={styles.container}>
@@ -11,8 +12,10 @@ const MainProfileLayout = ({ data }: { data: collectionObject }) => {
             </div>
             <div className={styles.profile_text_contents}>
                 <h4 className={`${styles.display_title} ${styles.display_text}`}>{data?.collection}</h4>
+                <h5>Collection by {data?.creator_id}</h5>
                 <p className={`${styles.display_description} ${styles.display_text}`}>{data?.description}</p>
             </div>
+            <Stats stats={data} />
         </div>
     );
 };
