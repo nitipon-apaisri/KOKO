c39d44cffcb0f273e9a20dbe50a5a4d3d7fe55e6
nitipon <nitipon.apaisri@gmail.com>
2022-11-08 22:18:52 +0100

    Added icon for any collections if the creator is verified also, added Link to nevigate to the collection view

--- components/SearchSuggestions/SearchSugesstions.tsx
+++ components/SearchSuggestions/SearchSugesstions.tsx
@@ -1,9 +1,12 @@
 import * as React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 import { onSearchCollectionsObject } from "../../@types/collection";
 import { Loading } from "../Loading";
 import styles from "../../styles/SearchSuggestions.module.css";
-import { Avatar, Space } from "antd";
+import { Avatar, Space, Tooltip } from "antd";
 import { concatMediaWithParasCDN } from "../../utils/modules";
+import Link from "next/link";
 const SearchSuggestions = ({ collectionsSearch }: { collectionsSearch: onSearchCollectionsObject[] }) => {
     return (
         <div className={styles.suggestions_wrapper}>
@@ -11,17 +14,22 @@ const SearchSuggestions = ({ collectionsSearch }: { collectionsSearch: onSearchC
                 <Loading />
             ) : (
                 collectionsSearch
-                    .sort((a, b) => (a.collection < b.collection ? -1 : 1))
+                    .sort((a, b) => (a.is_creator < b.is_creator ? -1 : 1))
                     .filter((v, i, a) => a.findIndex((v2) => v2._id === v._id) === i)
                     .map((r, i) => (
-                        <div key={r._id} className={`${styles.suggestion_item} ${styles.item_bt_border}`}>
-                            <Space>
-                                <Avatar src={`${concatMediaWithParasCDN(r.media.toString())}`} style={{ border: "1px solid #dfdfdf" }} />
-                                <h5>
-                                    {r.collection} by {r.creator_id}
-                                </h5>
-                            </Space>
-                        </div>
+                        <Link href={`collection/${r.collection_id}`} key={r._id}>
+                            <div className={`${styles.suggestion_item} ${styles.item_bt_border}`}>
+                                <Space>
+                                    <Avatar src={`${concatMediaWithParasCDN(r.media.toString())}`} style={{ border: "1px solid #dfdfdf" }} />
+                                    <h4>{r.collection}</h4>
+                                    {r.is_creator && (
+                                        <Tooltip title="Creator verified">
+                                            <FontAwesomeIcon icon={faCircleCheck} color="Dodgerblue" />
+                                        </Tooltip>
+                                    )}
+                                </Space>
+                            </div>
+                        </Link>
                     ))
             )}
         </div>
